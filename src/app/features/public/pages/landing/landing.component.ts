08860d1176b462d04238905f5c4ecb57fe882d3b
Fernando Rocha Olivera <frochaolivera18@hotmail.com>
2026-03-10 12:54:27 -0500

    feat: Update routes configuration

---
+++ src/app/features/public/pages/landing/landing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing.component.html',
+  styles: ``
+})
+export class LandingComponent {
+
+}
