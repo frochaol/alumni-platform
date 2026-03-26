740272f8bd314de776c32b2d8cbf4010413abcab
Fernando Rocha Olivera <frochaolivera18@hotmail.com>
2026-03-25 21:46:39 -0500

    feat: Implement a new styled public footer component and adjust global and public layout styles for full-height display.

--- src/app/core/layout/public-layout/public-footer/public-footer.ts
+++ src/app/core/layout/public-layout/public-footer/public-footer.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-public-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './public-footer.html',
   styleUrl: './public-footer.scss',
 })
-export class PublicFooter {}
+export class PublicFooter {
+  currentYear = new Date().getFullYear();
+}
